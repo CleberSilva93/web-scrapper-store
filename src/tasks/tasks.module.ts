0029fe4bc6e4d346671237e38f8984868e629bdd
CleberAugustz <cleber.augustz@hotmail.com>
2023-01-15 19:44:34 -0300

    feat: include handleCron on tasks

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ScrapperModule } from 'src/computers/scrapper/scrapper.module';
 import { TasksService } from './tasks.service';
 
 @Module({
   controllers: [],
   providers: [TasksService],
+  imports: [ScrapperModule],
 })
 export class TasksModule {}
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { Interval } from '@nestjs/schedule';
+import { ScrapperService } from 'src/computers/scrapper/scrapper.service';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  private readonly logger = new Logger(TasksService.name);
+
+  constructor(private scrapperService: ScrapperService) {}
+
+  @Interval(+process.env.INTERVAL_SCRAPPER_MS || 20000)
+  handleCron() {
+    const scrapperServiceInput = {
+      linkToScrapper:
+        'https://webscraper.io/test-sites/e-commerce/allinone/computers/laptops',
+      selector: '.thumbnail',
+      filterBy: 'lenovo',
+    };
+
+    this.scrapperService.handle(scrapperServiceInput);
+    this.logger.debug(
+      `${scrapperServiceInput.linkToScrapper} scrapped and filtered by ${scrapperServiceInput.filterBy}`,
+    );
+  }
+}
